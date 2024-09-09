c2ae66451a80f883d87cc1f92732a12972b62107
unknown <tamiloreolaniyi@gmail.com>
2024-09-09 11:26:58 +0100

    homepage

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,32 @@
-import React from 'react'
+import HeaderBox from "@/components/HeaderBox";
+import RightSideBar from "@/components/RightSideBar";
+import TotalBalanceBox from "@/components/TotalBalanceBox";
+import React from "react";
 
 const Home = () => {
+    const loggedIn = {firstName: "Tamilore", lastName: 'Olaniyi', email: 'tamiloreolaniyi@gmail.com'}
   return (
-    <div>
-      Home
-    </div>
-  )
-}
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox
+            type='greeting'
+            title='Welcome'
+            user={loggedIn?.firstName || "Guest"}
+            subtext = 'Access and Manage your account and trasactions efficiently'
+            />
+            <TotalBalanceBox
+              accounts={[]}
+              totalBanks = {1}
+              totalCurrentBalance = {1250.35}
+            />
+        </header>
 
-export default Home
+        RECENT TRANSACTIONS
+      </div>
+      <RightSideBar user={loggedIn} transactions={[]} banks={[{currentBalance: 123.50},{currentBalance: 500.50 }]}/>
+    </section>
+  );
+};
+
+export default Home;
